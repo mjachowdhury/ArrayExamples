9cd704d0a65575b35b1bb2562c43d71bda45bdad
Mohammed <study.computing2016@gmail.com>
2018-08-18 06:37:45 +0100

    Sorting Numeric and String Array

---
+++ SortNumericAndStringArray.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+/*
+ * Write a Java program to sort a numeric array and a string array.
+ */
+public class SortNumericAndStringArray {
+	
+	/**
+	 * This method will sort numeric array 
+	 * @param arrNum
+	 */
+	public static void sortNumericArray(int [] arrNum){
+		System.out.println("Before Sorting :" +Arrays.toString(arrNum));
+		Arrays.sort(arrNum);//sorting the array
+		System.out.println("After Sorting the Array :"+Arrays.toString(arrNum));//after sorting the array
+	}
+
+	
+	public static void sortStringArray(String [] arrStr){
+		System.out.println("Before Sorting :" +Arrays.toString(arrStr));
+		Arrays.sort(arrStr);//sorting the array
+		System.out.println("After Sorting the Array :"+Arrays.toString(arrStr));//after sorting the array
+	}
+	public static void main(String[] args) {
+		
+		
+		int myArray [] = {2,5,7,4,5,9,8};		 
+		sortNumericArray(myArray);//calling Numeric sort array method
+				
+		String myStringArray[] = {"a","b","d","a"};
+		sortStringArray(myStringArray);//calling String sort array method
+	}
+
+}
