70dad7fe44a0eee1fd40c9eada715784ecb2c7b9
Mohammed <study.computing2016@gmail.com>
2018-08-18 18:11:12 +0100

    REmove element from array

---
+++ RemoveSpecificElementFromArray.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+/**
+ * Java program to remove a specific element from an array.
+ * @author Mohammed
+ *
+ */
+public class RemoveSpecificElementFromArray {
+	
+	static void removeFromArray(int [] arrNum){
+		
+		int removeIndex = 0;//whatever number will pass that index number will be removed
+		for(int i = removeIndex; i< arrNum.length-1; i++){
+			arrNum[i] = arrNum[i+1];
+		}
+	}
+
+	public static void main(String[] args) {
+		 int myArr [] = {1,3,5,6,7,34,56,78};
+		 System.out.println("Origianl Array :" + Arrays.toString(myArr));
+		 
+		 // We cannot alter the size of an array , after the removal, the last and second last element in the array will exist twice
+		 
+		 removeFromArray(myArr);
+		 System.out.println("After Removing :" +Arrays.toString(myArr));
+
+	}
+
+}
