5fad7df2bcc8dc894dda966c24782d3d1d4928a9
Mohammed <study.computing2016@gmail.com>
2018-08-18 18:47:32 +0100

    Insert an item into the index position of an array

---
+++ InsertAnElementToArray.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+/**
+ * Java program to insert an element (specific position) into an array.
+ * @author Mohammed
+ *
+ */
+public class InsertAnElementToArray {
+
+	/**
+	 * This method will put new value to the index position.
+	 * @param arrNum
+	 */
+	static void intertAnElementIntoArray(int[] arrNum) {
+		System.out.println("Original Array :" + Arrays.toString(arrNum));
+		int indexPosition = 2;//Declare the index position
+		int newValue = 5;// Declare new value 
+		arrNum[indexPosition] = newValue;
+		System.out.println("After putting new value to the index :" + Arrays.toString(arrNum));
+	}
+
+	//Main method
+	public static void main(String[] args) {
+		int myArr[] = { 1, 2, 3, 4, 5 };
+		intertAnElementIntoArray(myArr);
+	}
+
+}
